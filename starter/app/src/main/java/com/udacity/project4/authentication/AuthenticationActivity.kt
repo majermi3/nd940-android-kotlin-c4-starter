457fb19bef85aa647235d330b751245e8fac5249
Michal Majernik <majernik.miso@gmail.com>
2022-01-23 12:07:11 -0600

    [TASK] Redirect logged in user to RemindersActivity when returned from Login

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,10 +1,13 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.RemindersActivity
@@ -17,6 +20,7 @@ import com.udacity.project4.locationreminders.RemindersActivity
 class AuthenticationActivity : AppCompatActivity() {
 
     companion object {
+        private const val TAG = "AuthenticationActivity"
         const val SIGN_IN_REQUEST_CODE = 101
     }
 
@@ -31,7 +35,8 @@ class AuthenticationActivity : AppCompatActivity() {
         }
 
         //  If the user was authenticated, send him to RemindersActivity
-        if (FirebaseAuth.getInstance().currentUser != null) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
             navigateToRemindersActivity()
         }
 
@@ -58,5 +63,17 @@ class AuthenticationActivity : AppCompatActivity() {
         val intent = Intent(applicationContext, RemindersActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                navigateToRemindersActivity()
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
     }
 }
