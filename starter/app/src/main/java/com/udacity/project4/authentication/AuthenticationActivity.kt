606f71600a395e9d203178675a3379e94a247e6e
Michal Majernik <majernik.miso@gmail.com>
2022-01-24 16:02:01 -0600

    [TASK] Add custom layout to the login screen

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -6,12 +6,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.locationreminders.RemindersActivity
-
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -39,10 +39,6 @@ class AuthenticationActivity : AppCompatActivity() {
         if (currentUser != null) {
             navigateToRemindersActivity()
         }
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-
     }
 
     private fun launchSignInFlow() {
@@ -50,10 +46,16 @@ class AuthenticationActivity : AppCompatActivity() {
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.custom_authentication)
+            .setGoogleButtonId(R.id.sign_in_google)
+            .setEmailButtonId(R.id.sign_in_email)
+            .build()
+
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
+                .setAuthMethodPickerLayout(customLayout)
                 .build(),
             SIGN_IN_REQUEST_CODE
         )
