b60f81499557746894d63bdd81f20dc26a1437ec
Michal Majernik <majernik.miso@gmail.com>
2022-01-25 16:29:47 -0600

    [TASK] Add tests for RemindersListViewModel

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,27 +2,31 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import java.util.LinkedHashMap
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: LinkedHashMap<String, ReminderDTO> = LinkedHashMap() ) : ReminderDataSource {
 
 //    TODO: Create a fake data source to act as a double to the real data source
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (reminders.isEmpty()) return Result.Error("No Data")
+        return Result.Success(reminders.values.toList())
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders[reminder.id] = reminder
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val reminderDto = reminders[id]
+        if (reminderDto != null) {
+            return Result.Success(reminderDto)
+        }
+        return Result.Error("Not found")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
-
-
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,86 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.LinkedHashMap
+import org.hamcrest.Matchers.*
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private val reminder1 = ReminderDTO("Reminder 1", "Description 1", "Location 1", 48.1, 50.1, "reminder1")
+    private val reminder2 = ReminderDTO("Reminder 2", "Description 2", "Location 2", 48.2, 50.2, "reminder2")
+    private val reminder3 = ReminderDTO("Reminder 3", "Description 3", "Location 3", 48.3, 50.3, "reminder3")
+    private val reminders = LinkedHashMap<String, ReminderDTO>()
 
+    private lateinit var dataSource: FakeDataSource
+    private lateinit var reminderListViewModel: RemindersListViewModel
+
+    @Test
+    fun loadReminders_setsReminderList() {
+        // When reminders are added to the list
+        reminders[reminder1.id] = reminder1
+        reminders[reminder2.id] = reminder2
+        reminders[reminder3.id] = reminder3
+
+        dataSource = FakeDataSource(reminders)
+        reminderListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            dataSource
+        )
+        // Then loadReminders is called
+        reminderListViewModel.loadReminders()
+
+        // Verify that reminderList has correct data and there is no error
+        assertThat(reminderListViewModel.remindersList.value, not(nullValue()))
+        assertThat(reminders.size, `is`(reminderListViewModel.remindersList.value?.size))
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.title, `is`(reminders[reminder1.id]!!.title))
+        assertThat(reminderListViewModel.remindersList.value?.get(1)?.title, `is`(reminders[reminder2.id]!!.title))
+        assertThat(reminderListViewModel.remindersList.value?.get(2)?.title, `is`(reminders[reminder3.id]!!.title))
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.description, `is`(reminders[reminder1.id]!!.description))
+        assertThat(reminderListViewModel.remindersList.value?.get(1)?.description, `is`(reminders[reminder2.id]!!.description))
+        assertThat(reminderListViewModel.remindersList.value?.get(2)?.description, `is`(reminders[reminder3.id]!!.description))
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.location, `is`(reminders[reminder1.id]!!.location))
+        assertThat(reminderListViewModel.remindersList.value?.get(1)?.location, `is`(reminders[reminder2.id]!!.location))
+        assertThat(reminderListViewModel.remindersList.value?.get(2)?.location, `is`(reminders[reminder3.id]!!.location))
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.longitude, `is`(reminders[reminder1.id]!!.longitude))
+        assertThat(reminderListViewModel.remindersList.value?.get(1)?.longitude, `is`(reminders[reminder2.id]!!.longitude))
+        assertThat(reminderListViewModel.remindersList.value?.get(2)?.longitude, `is`(reminders[reminder3.id]!!.longitude))
+
+        assertThat(reminderListViewModel.remindersList.value?.get(0)?.latitude, `is`(reminders[reminder1.id]!!.latitude))
+        assertThat(reminderListViewModel.remindersList.value?.get(1)?.latitude, `is`(reminders[reminder2.id]!!.latitude))
+        assertThat(reminderListViewModel.remindersList.value?.get(2)?.latitude, `is`(reminders[reminder3.id]!!.latitude))
+
+        assertThat(reminderListViewModel.showSnackBar.value, nullValue())
+    }
+
+    @Test
+    fun loadReminders_withEmptyData_ShowSnackBarHasErrorNoData() {
+        // When no reminders are added to the list
+        dataSource = FakeDataSource(reminders)
+        reminderListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            dataSource
+        )
+
+        // Then loadReminders is called
+        reminderListViewModel.loadReminders()
+
+        // Verify that error massage is in the snackBar
+        assertThat(reminderListViewModel.showSnackBar.value, `is`("No Data"))
+    }
 }
