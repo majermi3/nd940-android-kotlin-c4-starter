25a7edb8ee93fd742f48873ff96ff10f235c23f3
Michal Majernik <majernik.miso@gmail.com>
2022-01-30 12:37:45 -0600

    [TASK] Verify that a snackbar with error message is displayed when invalid data is persisted

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -21,6 +22,8 @@ import com.udacity.project4.util.DataBindingIdlingResource
 import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -118,17 +121,39 @@ class RemindersActivityTest :
         // Verify that title, description and location are set
         onView(withId(R.id.reminderTitle)).check(matches(withText("Title")))
         onView(withId(R.id.reminderDescription)).check(matches(withText("Description")))
-        onView(withId(R.id.selectedLocation)).check(matches(withText("Dropped Pin")))
+        onView(withId(R.id.selectedLocation)).check(matches(withText(R.string.dropped_pin)))
 
         // Persist the reminder
         onView(withId(R.id.saveReminder)).perform(click())
 
+        // Verify that success Toast was displayed
+        onView(withText(appContext.getString(R.string.reminder_saved)))
+                .inRoot(withDecorView(not(dataBindingIdlingResource.activity.window.decorView)))
+                .check(matches(isDisplayed()));
+
         // Verify that the reminder is listed
         onView(withText("Title")).check(matches(isDisplayed()))
         onView(withText("Description")).check(matches(isDisplayed()))
-        onView(withText("Dropped Pin")).check(matches(isDisplayed()))
+        onView(withText(R.string.dropped_pin)).check(matches(isDisplayed()))
 
         activityScenario.close()
     }
 
+    @Test
+    fun persistReminder_withInvalidData_showErrorSnackbar() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Open Add reminder view
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // Try to persist the reminder
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // Verify that success Toast was displayed
+        onView(allOf(withId(R.id.snackbar_text), withText(appContext.getString(R.string.err_enter_title))))
+                .check(matches(isDisplayed()));
+
+        activityScenario.close()
+    }
 }
